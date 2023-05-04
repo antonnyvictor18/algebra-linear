f3b62f3716b8ec3c496a0cd0552e6a22a7b501da
antonnyvictor <antonny.victor.silva@gmail.com>
2023-05-04 19:34:54 -0300

    ajustes no task 1

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Algebra_Linear
 Códigos de trabalhos práticos da disciplina Álgebra Linear Computacional
+
+
+Digite a ordem da matriz: 4
+Digite os valores da matriz A:
+5
+-4
+1
+0
+-4
+6
+-4
+1
+1
+-4
+6
+-4
+0
+1
+-4
+5
+Digite os valores do vetor b:
+-1
+2
+1
+3
+Matriz A: 
+5 -4 1 0 
+-4 6 -4 1 
+1 -4 6 -4 
+0 1 -4 5 
+Matriz L: 
+1 0 0 0 
+-0.8 1 0 0 
+0.2 -1.14286 1 0 
+0 0.357143 -1.33333 1 
+Matriz U: 
+5 -4 1 0 
+0 2.8 -3.2 1 
+0 0 2.14286 -2.85714 
+0 0 0 0.833333 
+A solução do sistema é:
+x1 = 5.8
+x2 = 10.2
+x3 = 10.8
+x4 = 7.2
--- task_01.cpp
+++ task_01.cpp
@@ -1,177 +1,193 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-
 using namespace std;
 
-// Função para imprimir a matriz
-void print_matrix(vector<vector<double>> &A, int n) {
+void imprimirMatriz(vector<vector<double>>& matriz) {
+    int n = matriz.size();
+    int m = matriz[0].size();
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << A[i][j] << "\t";
+        for (int j = 0; j < m; j++) {
+            cout << matriz[i][j] << " ";
         }
-        cout << endl;
+        cout << "\n";
     }
-    cout << endl;
 }
 
-// Função para imprimir o vetor
-void print_vector(vector<double> &x, int n) {
-    for (int i = 0; i < n; i++) {
-        cout << x[i] << endl;
-    }
-    cout << endl;
-}
 
-// Função para decompor a matriz A em LU
-void LU_decomposition(vector<vector<double>> &A, vector<vector<double>> &L, vector<vector<double>> &U, int n) {
+vector<vector<double>> decomposicaoCholesky(vector<vector<double>>& A, vector<vector<double>>& L, int n) {
+    // Calcular a decomposição de Cholesky
     for (int i = 0; i < n; i++) {
-        // Calcula os elementos da matriz L
         for (int j = 0; j <= i; j++) {
             double s = 0;
             for (int k = 0; k < j; k++) {
-                s += L[i][k] * U[k][j];
+                s += L[i][k] * L[j][k];
             }
-            L[i][j] = A[i][j] - s;
-        }
-        // Calcula os elementos da matriz U
-        for (int j = i + 1; j < n; j++) {
-            double s = 0;
-            for (int k = 0; k < i; k++) {
-                s += L[i][k] * U[k][j];
+            if (i == j) {
+                L[i][j] = sqrt(A[i][i] - s);
+            } else {
+                L[i][j] = (1.0 / L[j][j]) * (A[i][j] - s);
             }
-            U[i][j] = (A[i][j] - s) / L[i][i];
         }
-        L[i][i] = 1; // Define o elemento diagonal da matriz L como 1
     }
+
+    return L;
 }
 
-// Função para resolver o sistema linear de equações utilizando a decomposição LU
-void LU_solver(vector<vector<double>> &L, vector<vector<double>> &U, vector<double> &B, vector<double> &x, int n) {
-    vector<double> y(n);
-    // Resolve o sistema Ly = B
+// Função para decompor a matriz A em L e U
+void decomposicaoLU(vector<vector<double>>& A, vector<vector<double>>& L, vector<vector<double>>& U, int n) {
+    // Inicializa L com a matriz identidade e U com a matriz A
     for (int i = 0; i < n; i++) {
-        double s = 0;
+        L[i][i] = 1.0;
+        for (int j = 0; j < n; j++) {
+            U[i][j] = A[i][j];
+        }
+    }
+
+    // Executa o processo de eliminação de gauss para obter L e U
+    for (int k = 0; k < n; k++) {
+        for (int i = k + 1; i < n; i++) {
+            L[i][k] = U[i][k] / U[k][k];
+            for (int j = k; j < n; j++) {
+                U[i][j] = U[i][j] - L[i][k] * U[k][j];
+            }
+        }
+    }
+
+}
+
+// Função para resolver o sistema linear Ax = b usando a decomposição LU
+vector<double> resolverSistemaLU(vector<vector<double>>& A, vector<vector<double>>& L, vector<vector<double>>& U, vector<double>& b, int n) {
+    // Encontra a solução de Ly = b
+    vector<double> y(n, 0.0);
+    for (int i = 0; i < n; i++) {
+        double soma = 0.0;
+        for (int j = 0; j < i; j++) {
+            soma += L[i][j] * y[j];
+        }
+        y[i] = b[i] - soma;
+    }
+
+    // Encontra a solução de Ux = y
+    vector<double> x(n, 0.0);
+    for (int i = n - 1; i >= 0; i--) {
+        double soma = 0.0;
+        for (int j = i + 1; j < n; j++) {
+            soma += U[i][j] * x[j];
+        }
+        x[i] = (y[i] - soma) / U[i][i];
+    }
+
+    return x;
+}
+
+vector<double> resolverSistemaCholesky(vector<vector<double>>& A, vector<vector<double>>& L, vector<double>& B, int n) {
+    vector<double> y(n, 0.0);
+    vector<double> x(n, 0.0);
+
+    // Resolver Ly = B usando substituição direta
+    for (int i = 0; i < n; i++) {
+        double s = 0.0;
         for (int j = 0; j < i; j++) {
             s += L[i][j] * y[j];
         }
-        y[i] = (B[i] - s) / L[i][i];
+        y[i] = (1.0 / L[i][i]) * (B[i] - s);
     }
-    // Resolve o sistema Ux = y
-    for (int i = n - 1; i >= 0; i--) {
-        double s = 0;
-        for (int j = i + 1; j < n; j++) {
-            s += U[i][j] * x[j];
-        }
-        x[i] = (y[i] - s) / U[i][i];
-    }
-}
 
-// Função para decompor a matriz A em Cholesky
-void Cholesky_decomposition(vector<vector<double>> &A, vector<vector<double>> &L, int n) {
-    for (int i = 0; i < n; i++) {
-    // Calcula os elementos da matriz L
-    for (int j = 0; j <= i; j++) {
-        double s = 0;
-        for (int k = 0; k < j; k++) {
-            s += L[i][k] * L[j][k];
-        }
-        if (i == j) {
-            L[i][i] = sqrt(A[i][i] - s);
-        } else {
-            L[i][j] = (A[i][j] - s) / L[j][j];
-        }
-        }
-    }
-}
-// Função para resolver o sistema linear de equações utilizando a decomposição de Cholesky
-void Cholesky_solver(vector<vector<double>> &L, vector<double> &B, vector<double> &x, int n) {
-    vector<double> y(n);
-    // Resolve o sistema Ly = B
-    for (int i = 0; i < n; i++) {
-        double s = 0;
-        for (int j = 0; j < i; j++) {
-            s += L[i][j] * y[j];
-        }
-
-        y[i] = (B[i] - s) / L[i][i];
-    }
-    // Resolve o sistema L^Tx = y
+    // Resolver L^T x = y usando substituição reversa
     for (int i = n - 1; i >= 0; i--) {
-        double s = 0;
+        double s = 0.0;
         for (int j = i + 1; j < n; j++) {
             s += L[j][i] * x[j];
         }
-        x[i] = (y[i] - s) / L[i][i];
+        x[i] = (1.0 / L[i][i]) * (y[i] - s);
     }
+
+    return x;
 }
 
+
+
+
+// Função principal
 int main() {
-    int n;
-    cout << "Digite o tamanho da matriz A: ";
+    int n, ICOD, sair;
+    
+    cout << "Digite a ordem da matriz: ";
     cin >> n;
-    // Aloca as matrizes A, L e U e o vetor B
-vector<vector<double>> A(n, vector<double>(n));
-vector<vector<double>> L(n, vector<double>(n));
-vector<vector<double>> U(n, vector<double>(n));
-vector<double> B(n);
-vector<double> x(n);
 
-// Preenche a matriz A e o vetor B
-cout << "Digite os elementos da matriz A:" << endl;
-for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-        cin >> A[i][j];
-    }
-}
-cout << "Digite os elementos do vetor B:" << endl;
-for (int i = 0; i < n; i++) {
-    cin >> B[i];
-}
+    // Cria as matrizes A, L e U e o vetor b
+    vector<vector<double>> A(n, vector<double>(n, 0.0));
+    vector<vector<double>> L(n, vector<double>(n, 0.0));
+    vector<vector<double>> U(n, vector<double>(n, 0.0));
+    vector<double> b(n, 0.0);
+    vector<double> x;
 
-int ICOD;
-cout << "Digite o código do método a ser utilizado (1 para LU e 2 para Cholesky): ";
-cin >> ICOD;
-
-if (ICOD == 1) {
-    // Realiza a decomposição LU da matriz A
-    LU_decomposition(A, L, U, n);
-} else if (ICOD == 2) {
-    // Realiza a decomposição de Cholesky da matriz A
-    Cholesky_decomposition(A, L, n);
-} else {
-    cout << "Código inválido." << endl;
-    return 0;
-}
-
-// Resolve o sistema linear de equações para vários vetores B
-while (true) {
-    cout << "Digite os elementos do vetor B ou -1 para sair:" << endl;
-    bool flag = false;
+    // Lê os valores de A e b
+    cout << "Digite os valores da matriz A:\n";
     for (int i = 0; i < n; i++) {
-        cin >> B[i];
-        if (B[i] == -1) {
-            flag = true;
-            break;
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
         }
     }
-    if (flag) {
-        break;
-    }
-    if (ICOD == 1) {
-        // Resolve o sistema linear de equações utilizando a decomposição LU
-        LU_solver(L, U, B, x, n);
-    } else if (ICOD == 2) {
-        // Resolve o sistema linear de equações utilizando a decomposição de Cholesky
-        Cholesky_solver(L, B, x, n);
-    }
-    // Imprime o resultado
-    cout << "A solução do sistema é: ";
-    for (int i = 0; i < n; i++) {
-        cout << x[i] << " ";
-    }
-    cout << endl;
-}
 
-return 0;
+    cout << "Escolha o método de resolução (Decomposição LU (ICOD =1) ou Decomposição de Cholesky (ICOD =2)): ";
+    cin >> ICOD;
+
+    if (ICOD == 1){
+        // Executa a decomposição LU
+        decomposicaoLU(A, L, U, n);
+        while (true){
+            cout << "Digite os novoss valores do vetor b:\n";
+            for (int i = 0; i < n; i++) {
+                cin >> b[i];
+            }
+
+            // Resolve o sistema linear Ax = b usando a decomposição LU
+            x = resolverSistemaLU(A, L, U, b, n);
+            cout << "A solução do sistema é:\n";
+            for (int i = 0; i < n; i++) {
+                cout << "x" << i + 1 << " = " << x[i] << "\n";
+            }
+
+            cout << "desaja sair ? ('1' para sim ou '0' para não) ";
+            cin >> sair;
+
+            if (sair == 1){
+                return 0;
+            }
+            
+        }
+                
+    }   
+        
+        
+    else if (ICOD == 2){
+        decomposicaoCholesky(A,L,n);
+        while (true){
+            cout << "Digite os novoss valores do vetor b:\n";
+            for (int i = 0; i < n; i++) {
+                cin >> b[i];
+            }
+            // Resolve o sistema linear Ax = b usando a decomposição LU
+            x = resolverSistemaCholesky(A,L,b,n);
+            cout << "A solução do sistema é:\n";
+            for (int i = 0; i < n; i++) {
+                cout << "x" << i + 1 << " = " << x[i] << "\n";
+            }
+
+            cout << "desaja sair ? ('1' para sim ou '0' para não) ";
+            cin >> sair;
+
+            if (sair == 1){
+                return 0;
+            }
+        }
+    }
+    else {
+        cerr << "ICOD não definido"<<endl;
+        return EXIT_FAILURE;
+    }
+    
+    return 0;
 }
